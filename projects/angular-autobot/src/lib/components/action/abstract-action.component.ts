b1b25dc1517bffc72d44f8bbfd27a75395918e7f
malfusion <judenaveenraj@gmail.com>
2019-11-07 19:48:58 -0500

    Deleted everything in branch

--- projects/angular-autobot/src/lib/components/action/abstract-action.component.ts
+++
@@ -1,9 +0,0 @@
-import { Input } from '@angular/core';
-import { Action } from '../../models';
-
-export class AbstractActionComponent {
-  @Input() action: Action;
-  @Input() resolver: (any) => void;
-
-  constructor() {}
-}
--- projects/angular-autobot/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-autobot
- */
-
-export * from './lib/angular-autobot.service';
-export * from './lib/components/chatbot/autobot-chat.component';
-export * from './lib/angular-autobot.module';
